ed14aa07473eff5306a7ab7d3e52c177cd6320a6
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-06 11:05:29 -0300

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const userList =
+    [
+        {
+            nome: 'Gabryel',
+            username: 'gb.'
+        },
+        {
+            nome: 'Lucas',
+            username: 'Lorem'
+        },
+        {
+            nome: 'Julianna',
+            username: 'julie'
+        }
+    ];
+
+userList.push({
+    nome: 'Guilherme',
+    username: 'GUI!'
+});
+
+
+console.log(userList);
