13d1d0b93edef2dd1e16cb0ff6b269834ff618d8
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-07 13:03:56 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,3 @@
-const userList =
-    [
-        {
-            name: 'Gabryel',
-            username: 'gb.'
-        },
-        {
-            name: 'James',
-            username: 'J'
-        },
-        {
-            name: 'Claire',
-            username: 'cleire'
-        }
-    ]
+let vel = "80";
 
-const newUser =
-{
-    name: 'Gabryel',
-    username: 'gb.'
-}
-
-const hasUser = userList.find((user) => {
-    return user.username === newUser.username
-})
-if (!hasUser) {
-    userList.push(newUser)
-}
-const resultado = userList
-
-console.log(userList)
+console.log(vel === 80);
