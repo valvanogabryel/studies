487df6f69d40ef36298c92b911636ea4996607cf
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-07 16:01:06 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-let vel = "80";
+let spaceship = 'Falcon-9';
 
-console.log(vel === 80);
+const isFalcon = spaceship === 'Falcon-9';
+
+if (!isFalcon) {
+    const vel = 80;
+    console.log(`Your spaceship is ${spaceship}, witch has a velocity of ${vel}Km/h.`);
+    console.log(vel >= 100 ? 'Yes' : 'No');
+} else {
+    const vel = 100;
+    console.log(`Your spaceship is ${spaceship}, witch has a velocity of ${vel}Km/h`);
+    console.log(vel >= 100 ? 'Yes' : 'No');
+};
+
+
+
