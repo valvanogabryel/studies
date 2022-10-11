b5a0a7d5422c486bd5bd43b6d75e7e894548d02b
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-11 14:17:57 -0300

    ;

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
+function doubleVelocity(velocity, printer) {
+    console.log('Entrei em doubleVelocity')
+    let newVelocity = velocity * 2
+    printer(newVelocity)
+    return newVelocity
+}
 
+let printVelocity = velocity => {
+    console.log(`Estamos a ${velocity}Km/s`)
+}
+
+let newVelocity = doubleVelocity(70, printVelocity)
+
+console.log(newVelocity)
