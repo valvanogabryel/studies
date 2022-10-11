fd245fc3a0669e5ea6b3a3f77f2008805671130a
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-11 10:06:57 -0300

    exc006_done

---
+++ oneBitAulas/JS/exercicios/exc006/script.js
@@ -0,0 +1,11 @@
+let shipName = prompt('Digite o nome da nave: ');
+let invertedShipName = ''
+
+for (let i = shipName.length - 1; i >= 0; i--) {
+    if (shipName[i] == 'e') {
+        break
+    }
+    invertedShipName += shipName[i]
+}
+
+alert(`Nome original da nave: ${shipName}.\n\n Nome após a ocultação: ${invertedShipName}`)
