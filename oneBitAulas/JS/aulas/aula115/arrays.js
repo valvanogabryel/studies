04476bb1c3e1e8de49f155ef45277860c619b0c8
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-12 13:13:18 -0300

    Create arrays.js

---
+++ oneBitAulas/JS/aulas/aula115/arrays.js
@@ -0,0 +1,32 @@
+let hitchedSpaceships = ['Colossus', 'Supernova', 'Helmet']
+
+console.log(hitchedSpaceships);
+
+//pop()
+
+// A function '.pop()' remove o ultimo elemento de um Array. Também é possível atribuir o elemento removido dentro de uma variável.
+let removedSpaceships = hitchedSpaceships.pop()
+
+console.log(removedSpaceships)
+
+//shift()
+
+//Remove o primeiro elemento de um Array => [0]
+
+hitchedSpaceships.shift()
+
+console.log(hitchedSpaceships)
+
+//unshift()
+
+//Readiciona um elemento. Ou cria um elemento no primeiro índice do Array
+
+hitchedSpaceships.unshift('Fenix')
+
+console.log(hitchedSpaceships)
+
+//length()
+
+
+
+//indexOf()
