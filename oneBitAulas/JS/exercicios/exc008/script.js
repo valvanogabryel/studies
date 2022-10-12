cdbe18e0b932eb1d044c7174820c6f8ec01bf2c2
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-12 11:37:24 -0300

    exc007_done

--- oneBitAulas/JS/exercicios/exc008/script.js
+++ oneBitAulas/JS/exercicios/exc008/script.js
@@ -1,15 +1,25 @@
-function desaccelerate(velocity, printer) {
+function desaccelerate(brake, printer) {
     let shipVelocity = 150;
+    alert(`A velocidade atual é de ${shipVelocity}Km/s. Diminuindo...`)
+    // Estrutura de repetição para diminuir a velocidade. Cada iteração é como se fosse 1 segundo.
+    while (shipVelocity > 0) {
+        shipVelocity -= brake
+        if (shipVelocity < 0) {
+            shipVelocity = 0
+        }
+        // Callback de impressão de alert
+        printer(shipVelocity)
+    }
+    return shipVelocity
+}
 
-    let newVelocity = shipVelocity - velocity
-
-
-    printer(newVelocity);
-
-    return (newVelocity)
+let printVelocity = shipVelocity => {
+    alert(`Diminuindo a velocidade: ${shipVelocity}Km/s`)
+    if (shipVelocity == 0) {
+        alert(`Espaçonave parada. Comportas abrindo.`)
+    }
 }
 
 
-desaccelerate(50, () => {
-    console.log(`Estamos diminuindo: velocidade ${velocity}`)
-})
+
+desaccelerate(20, printVelocity)
