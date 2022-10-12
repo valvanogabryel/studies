89a12d1e588cd228b0d4feac24d388922ca6ea4d
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-12 13:23:44 -0300

    Update arrays.js

--- oneBitAulas/JS/aulas/aula115/arrays.js
+++ oneBitAulas/JS/aulas/aula115/arrays.js
@@ -30,3 +30,15 @@ console.log(hitchedSpaceships)
 
 
 //indexOf()
+
+//------------------------------------//
+//Arrays também podem agrupar outros arrays (que também podem agrupar outros arrays).
+
+let spaceships = [['Supernova', 10, [true, false]], ['Golias', 5, [true, false]], ['Colossus', 4, [true, false]], ['Helmet', 8, [true, false]]]
+
+console.log(spaceships[0][2][0])
+/*
+vai imprimir 'true'. Ou seja, ele entra no primeiro índice do Array maior, depois no segundo índice do primeiro Array, depois no primeiro índice do último Array.
+*/
+
+console.log(spaceships[3][2][1]) //vai imprimir 'false'
