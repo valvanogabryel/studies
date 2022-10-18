241a7c84ad7ba3a9994b22ce6f68d86542e22362
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-18 11:14:16 -0300

    created_exc011

--- oop.js
+++ oop.js
@@ -6,3 +6,42 @@ class SpacialStation {
 }
 
 let observatory = new SpacialStation('Observatório', 40)
+
+console.log(observatory)
+
+// ----------------------------------------------------------------------- //
+
+class Spaceship {
+    constructor(name, type = 'Batalha') {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+const ship1 = new Spaceship('Helmet', 'Descoberta')
+const ship2 = new Spaceship('Supernova')
+
+console.log(ship1)
+console.log(ship2)
+
+
+// ----------------------------------------------------------------------- //
+
+
+class Spaceship2 {
+    constructor(name) {
+        this.name = name;
+        this.velocity = 0;
+    } speedUp(acceleration) {
+        this.velocity += acceleration
+    }
+}
+
+
+const artemis = new Spaceship2('Artemis')
+
+artemis.speedUp(10)
+
+artemis.speedUp(15)
+
+console.log(artemis)
