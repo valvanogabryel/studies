1d2c7108db47ffe23a6e05aa437f1155690d8fef
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-18 11:41:45 -0300

    Update script.js

--- oneBitAulas/JS/exercicios/exc011/script.js
+++ oneBitAulas/JS/exercicios/exc011/script.js
@@ -1,54 +1,45 @@
-class Spaceship {
+class Spaceships {
     constructor(name, crewQuantity) {
         this.name = name;
         this.crewQuantity = crewQuantity;
         this.isHitched = false;
         this.entranceDoorsOpen = false;
-    }
-    hitch() {
+    } hitch() {
         this.isHitched = true;
         this.entranceDoorsOpen = true
     }
 }
 
-
 function createSpaceship() {
     let spaceshipName = prompt('Digite o nome da nave: ')
-    let crewQuantity = prompt('Informe a quantidade de tripulantes: ')
-    let spaceship = new Spaceship(spaceshipName, crewQuantity)
+    let spaceshipCrew = prompt('Informa a quantidade de tripulantes na nave: ')
+    let spaceship = new Spaceships(spaceshipName, spaceshipCrew)
     return spaceship
 }
 
-function printShips(spaceships) {
-    let spaceshipList = '';
-    spaceships.forEach((spaceship, index) => {
-        spaceshipList += (index + 1) + "- " + spaceship.name + " (" + spaceship.crewQuantity + "tripulantes\n"
-    })
-    alert(spaceshipList)
-}
+
 
 function showMenu() {
     let chosenOption;
     do {
-        chosenOption = parseInt(prompt('Deseja:\n1- Realizar engate\n2- Imprimir naves na tela\n3- Sair do programa'));
+        chosenOption = parseInt(prompt('Deseja:\n\n1- Realizar engate\n2- Imprimir naves na tela\n 3- Encerrar o programa'))
         switch (chosenOption) {
             case 1:
                 let spaceshipToAdd = createSpaceship()
-                spaceshipToAdd.hitch()
-                hitchedSpaceships.push(spaceshipToAdd)
+
+
                 break
             case 2:
-                printShips(hitchedSpaceships)
+
                 break
-            case 3:
-                endProgram()
-                break
+
             default:
-                alert('Valor inválido')
+                alert('Valor inválido');
         }
+
+
+
+
     } while (chosenOption != 3)
+
 }
-
-let hitchedSpaceships = []
-
-showMenu()
