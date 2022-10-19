e2d6c287e2709f6a6453c7c1230a4490b2630909
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-19 12:17:44 -0300

    Update tests2.js

--- tests2.js
+++ tests2.js
@@ -18,7 +18,7 @@ for (let c = 0; c <= 10; c++) {
 } 
 */
 
-class Spaceship {
+/*class Spaceship {
     constructor(name, maxCrew, maxRecommendedVelocity) {
         this.name = name;
         this.maxCrew = maxCrew;
@@ -50,12 +50,57 @@ class DiscoverySpaceship extends Spaceship {
 let darwin = new DiscoverySpaceship('Darwin', 10, 200);
 let fenix = new BattleSpaceship('Fenix', 8, 240);
 
-darwin.speedUp(210)
-console.log(darwin)
-fenix.speedUp(230)
-console.log(fenix)
+darwin.speedUp(250);
+console.log(darwin);
+fenix.speedUp(230);
+console.log(fenix);
 
-darwin.stop()
+darwin.stop();
 
-fenix.stop()
+fenix.stop();
 
+class TransportSpaceship extends Spaceship {
+    speedUp() {
+        console.log('Naves de tranporte só aumentam a velocidade em 1km/s')
+        this.currentVelocity++
+    }
+}
+
+let tranportSpaceship = new TransportSpaceship('Transportadora', 4, 100);
+
+
+console.log(tranportSpaceship)*/
+
+
+class Spaceship {
+    constructor(name, maxCrew, maxRecommendedVelocity) {
+        this.name = name;
+        this.maxCrew = maxCrew;
+        this.maxRecommendedVelocity = maxRecommendedVelocity;
+        this.currentVelocity = 0;
+    }
+    speedUp(acceleration) {
+        this.currentVelocity += acceleration;
+        if (this.currentVelocity > this.maxRecommendedVelocity) {
+            console.log('VELOCIDADE MÁXIMA ULTRAPASSADA!!!')
+        }
+    }
+}
+
+class TransportSpaceship extends Spaceship {
+    constructor(name, maxCrew, maxRecommendedVelocity, maxLoadWeight) {
+        super(name, maxCrew, maxRecommendedVelocity, maxLoadWeight)
+        this.maxLoadWeight = maxLoadWeight;
+    }
+    speedUp(acceleration) {
+        acceleration /= 2
+        console.log(`Incrementando velocidade em ${acceleration} km/s`)
+        super.speedUp(acceleration)
+    }
+}
+
+let tranportSpaceship = new TransportSpaceship("Transportadora", 4, 100, 400);
+
+tranportSpaceship.speedUp(210)
+
+console.log(tranportSpaceship)
