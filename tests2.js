12d510dd89afa153ad30abf1374ddabdacc54291
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-10-19 11:54:18 -0300

    Update tests2.js

--- tests2.js
+++ tests2.js
@@ -1,4 +1,4 @@
-const fruits = ['Apple', 'Banana', 'Kiwi', 'Orange'];
+/* const fruits = ['Apple', 'Banana', 'Kiwi', 'Orange'];
 
 for (i of fruits) {
     console.log(i)
@@ -15,6 +15,47 @@ for (let c = 0; c <= 10; c++) {
         continue
     }
     console.log(c)
+} 
+*/
+
+class Spaceship {
+    constructor(name, maxCrew, maxRecommendedVelocity) {
+        this.name = name;
+        this.maxCrew = maxCrew;
+        this.maxRecommendedVelocity = maxRecommendedVelocity;
+        this.currentVelocity = 0;
+    }
+    speedUp(acceleration) {
+        this.currentVelocity += acceleration;
+        if (this.currentVelocity >= this.maxRecommendedVelocity) {
+            console.log('VELOCIDADE MÁXIMA ULTRAPASSADA!!!')
+        }
+    }
 }
 
+class BattleSpaceship extends Spaceship {
+    stop() {
+        this.currentVelocity = 0;
+        console.log('Recolhendo armas e parando a espaçonave.')
+    }
+}
+
+class DiscoverySpaceship extends Spaceship {
+    stop() {
+        this.currentVelocity = 0;
+        console.log('Recolhendo equipamento de amostras e parando a espaçonave.')
+    }
+}
+
+let darwin = new DiscoverySpaceship('Darwin', 10, 200);
+let fenix = new BattleSpaceship('Fenix', 8, 240);
+
+darwin.speedUp(210)
+console.log(darwin)
+fenix.speedUp(230)
+console.log(fenix)
+
+darwin.stop()
+
+fenix.stop()
 
