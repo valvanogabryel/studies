8df89b359f4c5c2324a9342829b7b9c08267d238
Gabryel Valvano <valvanogabryel@gmail.com>
2022-10-20 12:09:19 -0300

    Update tests2.js

--- tests2.js
+++ tests2.js
@@ -15,7 +15,7 @@ for (let c = 0; c <= 10; c++) {
         continue
     }
     console.log(c)
-} 
+}
 */
 
 /*class Spaceship {
@@ -72,35 +72,95 @@ let tranportSpaceship = new TransportSpaceship('Transportadora', 4, 100);
 console.log(tranportSpaceship)*/
 
 
-class Spaceship {
-    constructor(name, maxCrew, maxRecommendedVelocity) {
+// class Spaceship {
+//     constructor(name, maxCrew, maxRecommendedVelocity) {
+//         this.name = name;
+//         this.maxCrew = maxCrew;
+//         this.maxRecommendedVelocity = maxRecommendedVelocity;
+//         this.currentVelocity = 0;
+//     }
+//     speedUp(acceleration) {
+//         this.currentVelocity += acceleration;
+//         if (this.currentVelocity > this.maxRecommendedVelocity) {
+//             console.log('VELOCIDADE MÁXIMA ULTRAPASSADA!!!')
+//         }
+//     }
+// }
+
+// class TransportSpaceship extends Spaceship {
+//     constructor(name, maxCrew, maxRecommendedVelocity, maxLoadWeight) {
+//         super(name, maxCrew, maxRecommendedVelocity, maxLoadWeight)
+//         this.maxLoadWeight = maxLoadWeight;
+//     }
+//     speedUp(acceleration) {
+//         acceleration /= 2
+//         console.log(`Incrementando velocidade em ${acceleration} km/s`)
+//         super.speedUp(acceleration)
+//     }
+// }
+
+// let tranportSpaceship = new TransportSpaceship("Transportadora", 4, 100, 400);
+
+// tranportSpaceship.speedUp(210)
+
+// console.log(tranportSpaceship)
+
+
+// class TransportSpaceship {
+//     constructor(name) {
+//         this.name = name;
+//         this.currentVelocity = 0;
+//     }
+
+//     set velocity(newVelocity) {
+//         if (newVelocity > 120) {
+//             this.currentVelocity = 120;
+//         } else {
+//             this.currentVelocity = newVelocity;
+//         }
+//     }
+// }
+
+// const ship = new TransportSpaceship('Transportadora')
+
+// ship.velocity = 200
+
+// console.log(ship)
+
+
+// class ResourceProcessStation {
+//     constructor(name, monthlyProcessedLoad) {
+//         this.name = name;
+//         this.monthlyProcessedLoad = monthlyProcessedLoad;
+//     }
+
+//     get weeklyProcessedLoad() {
+//         return this.monthlyProcessedLoad / 4
+//     }
+// }
+
+// const resourceProcessed = new ResourceProcessStation('Gaia', 500);
+
+// console.log(resourceProcessed.weeklyProcessedLoad);
+
+// resourceProcessed.monthlyProcessedLoad = 600;
+
+// console.log(resourceProcessed.weeklyProcessedLoad)
+
+
+class ResourceProcessStation {
+    constructor(name, monthlyProcessing) {
         this.name = name;
-        this.maxCrew = maxCrew;
-        this.maxRecommendedVelocity = maxRecommendedVelocity;
-        this.currentVelocity = 0;
+        this.monthlyProcessing = monthlyProcessing;
     }
-    speedUp(acceleration) {
-        this.currentVelocity += acceleration;
-        if (this.currentVelocity > this.maxRecommendedVelocity) {
-            console.log('VELOCIDADE MÁXIMA ULTRAPASSADA!!!')
-        }
+
+    static calculateProcessInHours(monthlyProcessing, hours) {
+        return monthlyProcessing / 720 * hours
     }
 }
 
-class TransportSpaceship extends Spaceship {
-    constructor(name, maxCrew, maxRecommendedVelocity, maxLoadWeight) {
-        super(name, maxCrew, maxRecommendedVelocity, maxLoadWeight)
-        this.maxLoadWeight = maxLoadWeight;
-    }
-    speedUp(acceleration) {
-        acceleration /= 2
-        console.log(`Incrementando velocidade em ${acceleration} km/s`)
-        super.speedUp(acceleration)
-    }
-}
+const station = new ResourceProcessStation('Gaia', 2000)
 
-let tranportSpaceship = new TransportSpaceship("Transportadora", 4, 100, 400);
+let totalProcessed = Math.round(ResourceProcessStation.calculateProcessInHours(station.monthlyProcessing, 10));
 
-tranportSpaceship.speedUp(210)
-
-console.log(tranportSpaceship)
+console.log(totalProcessed);
