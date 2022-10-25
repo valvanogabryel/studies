98353ac43a431451e61f08883b9d182a6dca4d48
Gabryel Valvano <valvanogabryel@gmail.com>
2022-10-25 09:25:37 -0300

    ;

--- oneBitAulas/JS/exercicios/exc012_nohelp/transport_spaceship.js
+++
@@ -1,6 +0,0 @@
-class TransportSpaceship extends Spaceship {
-    constructor(name, crewQuantity, seatsQuantity) {
-        super(name, crewQuantity);
-        this.seatsQuantity = seatsQuantity;
-    }
-}
