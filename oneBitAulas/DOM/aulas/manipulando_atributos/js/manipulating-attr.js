9f4b535728693bb1ac55b37edf8e71e884831642
Gabryel Valvano <valvanogabryel@gmail.com>
2022-10-28 10:19:04 -0300

    Update manipulating-attr.js

--- oneBitAulas/DOM/aulas/manipulando_atributos/js/manipulating-attr.js
+++ oneBitAulas/DOM/aulas/manipulando_atributos/js/manipulating-attr.js
@@ -1,5 +1,16 @@
 function verifyEnablement() {
-
-
+    let selectedValue = document.querySelector('select').value;
+    let textElement = document.getElementById('message');
+    if (selectedValue == 'disabled') {
+        document.querySelector('input[name="some-text"]').disabled = true;
+        textElement.innerText = 'DESABILITADO';
+        textElement.style.color = 'white';
+        textElement.style.background = 'red';
+    } else {
+        document.querySelector('input[name="some-text"]').disabled = false;
+        textElement.innerText = '';
+        textElement.style.color = 'black';
+        textElement.style.background = 'transparent';
+    }
 
 }
