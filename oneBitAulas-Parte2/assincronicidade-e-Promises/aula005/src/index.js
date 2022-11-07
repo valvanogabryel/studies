bcc69ee102dc7d671836631fb0f0769091b088d3
valvanogabryel <valvanogabryel@gmail.com>
2022-11-07 11:01:35 -0300

    j

--- oneBitAulas-Parte2/assincronicidade-e-Promises/aula005/src/index.js
+++ oneBitAulas-Parte2/assincronicidade-e-Promises/aula005/src/index.js
@@ -1,7 +1,7 @@
 let komodoShip = {
     name: 'Komodo',
     velocity: 80,
-    acceleration: 10
+    acceleration: 0
 }
 
 const velocityAfter2Seconds = (velocity, acceleration) => {
@@ -19,9 +19,11 @@ const velocityAfter2Seconds = (velocity, acceleration) => {
     })
 }
 
-velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration).then(velocity => { // THEN() É CHAMADO QUANDO A PROMISE DÁ CERTO (RESOLVE())
+velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration).then(velocity => {
     komodoShip.velocity = velocity;
     console.log('Depois de acelerar:\n', komodoShip)
+}).catch(message => { //CATCH SÓ É CHAMADO QUANDO A PROMISE DÁ ERRO (REJECT())
+    console.log(`Komodo: ${message}`)
 })
 
 
