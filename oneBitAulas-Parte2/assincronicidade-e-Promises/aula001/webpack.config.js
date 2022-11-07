169c1a19384eec7a5d3d989cabdf75bd4d546839
valvanogabryel <valvanogabryel@gmail.com>
2022-11-07 09:58:48 -0300

    hi

---
+++ oneBitAulas-Parte2/assincronicidade-e-Promises/aula001/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    module: {
+        test: /\.js$/,
+        use: ['babel-loader']
+    },
+
+    devServer: {
+
+    }
+
+
+
+
+}
