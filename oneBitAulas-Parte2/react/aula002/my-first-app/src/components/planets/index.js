d0a5b0550d0c90b103d67ac3f8ea5d25342a72fa
valvanogabryel <valvanogabryel@gmail.com>
2022-11-21 14:36:29 -0300

    Update index.js

--- oneBitAulas-Parte2/react/aula002/my-first-app/src/components/planets/index.js
+++ oneBitAulas-Parte2/react/aula002/my-first-app/src/components/planets/index.js
@@ -10,7 +10,7 @@ const Planets = () => {
         <Fragment>
             <h3>Planet List</h3>
             <hr></hr>
-            <Planet name="Mercúrio" s
+            <Planet name="Mercúrio"
                 description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar."
                 img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
                 link="https://pt.wikipedia.org/wiki/Mercúrio_(planeta)"
